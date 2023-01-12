e61e5c7e742231c064c01f99d2404972d6cd2952
r4ghuveer <raghuveerofficial08@gmail.com>
2023-01-12 23:56:06 +0530

    12jan2023

---
+++ Isomorphic_strings.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+string s="egg";
+string t="add";
+if(s.size()!=t.size()){
+    return false;
+}
+unordered_map <char,char> umap;
+unordered_map <char,char> :: iterator itr=umap.begin();
+for(int i =0; i<s.size();i++){
+    if(umap.find(s[i])!=umap.end()){
+        itr=umap.find(s[i]);
+        if(t[i]!=itr->second){
+            cout<< "false";
+        }
+    }
+    else if(umap.empty()==false){
+        for(auto x:umap){
+            if(t[i]==x.second && s[i]!=x.first){
+                return false;
+            }
+            else{
+                umap[s[i]]=t[i];
+            }
+        }
+    }
+    else{
+        umap[s[i]]=t[i];
+
+    }
+
+}
+cout<< "true";
+}
+   
