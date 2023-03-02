f3fe5e6ed4f1e452ff0ab7e423e07991bcaafed9
r4ghuveer <raghuveerofficial08@gmail.com>
2023-03-03 04:59:20 +0530

    try folder

--- Try/cry.cpp
+++ Try/cry.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    vector<int> vec={1,2,3,4,5};
+    vector<int>:: iterator it=vec.begin()+1;
+    for(int i=1;i<3;i++){
+        vec.erase(it);
+        
+    }
+    it=vec.begin()+1;
+    vec.insert(it,32);
+    for(auto x : vec){
+        cout<<x<<" ";
+    }
 // 48  0 
 // 49  1         
 // 50  2         
@@ -11,15 +22,17 @@ int main(){
 // 55  7         
 // 56  8         
 // 57  9
-    int i=952;
-    string str=to_string(9);
-    str[0]='2';
-    str.push_back('3');
-    string str2;
-    str2.push_back('0');
-    str2.push_back('1');
-    cout<<stoi(str2)<<endl;
-    cout<<str;
+
+    // int i=952;
+    // string str=to_string(9);
+    // str[0]='2';
+    // str.push_back('3');
+    // string str2;
+    // str2.push_back('0');
+    // str2.push_back('1');
+    // cout<<stoi(str2)<<endl;
+    // cout<<str;
+
     // char x;
     // x=int('1');
     // cout<<x<<endl;
