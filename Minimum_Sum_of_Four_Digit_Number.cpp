c630efc13cb088018192928d64f08a9e37edf2b6
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-25 23:57:10 +0530

    -

--- Minimum_Sum_of_Four_Digit_Number.cpp
+++ Minimum_Sum_of_Four_Digit_Number.cpp
@@ -8,6 +8,7 @@ public:
             ans.push_back(k%10);
             k=k/10;
         }
+        
         sort(ans.begin(),ans.end());
         int sum=(ans[0]*10+ans[3])+ans[1]*10+ans[2];
         return sum;
