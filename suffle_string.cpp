f5d4c5965e50634d5e838237a2cbfee210bf2dc9
r4ghuveer <raghuveerofficial08@gmail.com>
2023-04-26 22:55:47 +0530

    -

---
+++ suffle_string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int siz = indices.size();
+        string ans(siz,'-');
+        int i=0;
+        for(auto x : indices){
+            ans[x]=s[i];
+            i++;
+        } 
+        return ans;
+    }
+};
